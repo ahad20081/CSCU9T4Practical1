bdb2c806f293d379c35d39932418e93e8c1e9539
ahad2 <ahad2@10.128.47.93>
2023-04-12 11:54:36 +0400

    commit at 11:55

--- src/main/java/com/stir/cscu9t4practical1/TrainingRecord.java
+++ src/main/java/com/stir/cscu9t4practical1/TrainingRecord.java
@@ -3,13 +3,16 @@ package com.stir.cscu9t4practical1;
 import java.util.*;
 
 public class TrainingRecord {
-private List<Entry> tr;
-public TrainingRecord() {
-} //constructor
-// add a record to the list
-public void addEntry(Entry e){
-tr.add(e);    
-  } // addClass
+    private List<Entry> tr;
+    
+    public TrainingRecord() {
+        tr = new ArrayList<Entry>();
+    } //constructor
+    
+    // add a record to the list
+   public void addEntry(Entry e){
+       tr.add(e);    
+   } // addClass
 // look up the entry of a given day and month
  public String lookupEntry (int d, int m, int y) {
  ListIterator<Entry> iter = tr.listIterator();
@@ -20,4 +23,42 @@ if (current.getDay()==d && current.getMonth()==m && current.getYear()==y)
  result = current.getEntry();
 }
 return result;
-} }// lookupEntry
+} // lookupEntry
+
+
+public String lookallEntry (int d, int m, int y) {
+ListIterator<Entry> iter = tr.listIterator();
+ String result = "No entries found";
+while (iter.hasNext()) {
+Entry current = iter.next();
+if (current.getDay()==d && current.getMonth()==m && current.getYear()==y)
+ result = result +current.getEntry();
+}
+return result;
+}
+
+
+public String removeEntry (String n, int d, int m, int y) {
+	ListIterator<Entry> iter = tr.listIterator();
+ String result = "No entries found";
+ while (iter.hasNext()) {
+Entry current = iter.next();
+if (current.getName().equals(n) && current.getDay()==d && current.getMonth()==m && current.getYear()==y)
+ result = current.getEntry();
+}
+ return result;
+ }
+public int getNumberOfEntries(){
+ return tr.size();
+ }
+// Clear all entries
+ public void clearAllEntries(){
+tr.clear();
+}
+
+public String RemoveEntry(String n, int d, int m, int y) {
+// TODO Auto-generated method stub
+return null;
+}
+
+} // TrainingRecord
